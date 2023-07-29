1d489a915831012ba4c41f6b4dcbe5dd8997ec40
juancabeza <juancabeza@localhost>
2023-07-28 23:13:25 -0400

    Initial Commit

---
+++ src/sport/FavSport.java
@@ -0,0 +1,11 @@
+package sport;
+
+public class FavSport {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My favorite sport is soccer...");
+		System.out.println("The next soccer World Cup will be in the USA");
+	}
+
+}
